1ec73bd79d9f2d6d5d0774325f16662a53cc308b
David Boles <ciccioboles@Davids-MBP.att.net>
2016-12-28 21:10:33 -0800

    adding new project to github

--- Pills/ViewController.swift
+++ Pills/ViewController.swift
@@ -8,18 +8,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    @IBOutlet weak var extraPracticeTitle: UILabel!
+    @IBOutlet weak var extraPracticeTextField: UITextField!
+    let states = ["California", "Texas", "Idaho", "New York", "Hawaii", "Utah"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
     }
-
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+        
+                
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        
+        statePicker.isHidden = false
+        extraPracticeTitle.isHidden = true
+        extraPracticeTextField.isHidden = true
+        
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState()) //.normal known bug in beta
+        statePicker.isHidden = true
+    }
+    
+//    @IBAction func buyNowBtn(_ sender: AnyObject) {
+//        for i in 1...14 {
+//            self.view.viewWithTag(i)?.isHidden = true
+//        }
+//        
+//    }
+    
+    
+//    @IBAction func buyNowButtonPressed(_ sender: Any) {
+//        for view in self.view.subviews as [UIView] {
+//            view.isHidden = true
+//        }
+//        successImage.isHidden = false
+//    }
+    
+    
 }
+
 
---
+++ Pills/secondVC.swift
@@ -0,0 +1,34 @@
+//
+//  secondVC.swift
+//  Pills
+//
+//  Created by David Boles on 12/28/16.
+//  Copyright © 2016 David Boles. All rights reserved.
+//
+
+import UIKit
+
+class secondVC: UIViewController {
+
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+}
